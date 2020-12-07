cb09fc97005a92b0e54365d5601a20ece55d6b7e
Jie Li <lijiemessage@gmail.com>
2020-12-07 14:55:22 +0800

    Finish Register

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "graphene_django",
     'tracks',
+    'corsheaders'
 ]
 GRAPHENE = {
   'SCHEMA': 'app.schema.schema',
@@ -48,6 +49,8 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +59,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+]
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
 ]
 
 AUTHENTICATION_BACKENDS = [
